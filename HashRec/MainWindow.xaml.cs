6970f9d757c66cb7d4a9f5be2706225ebbca2cfe
Amanuel Teferi <aman.teferi.80@gmail.com>
2019-11-10 07:31:30 -0900

    feat (drag and drop) added drag and drop feature and fixed the design of the User Interface

--- HashRec/MainWindow.xaml.cs
+++ HashRec/MainWindow.xaml.cs
@@ -20,182 +20,189 @@ using System.IO;
 
 namespace HashRec
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        UnicodeEncoding ByteConverter = new UnicodeEncoding();
-        public static RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
-        string publicKey = RSA.ToXmlString(false);
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		UnicodeEncoding ByteConverter = new UnicodeEncoding();
+		public static RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+		string publicKey = RSA.ToXmlString(false);
 
-        string senderPublicKey; 
+		string senderPublicKey; 
 
-        public MainWindow()
-        {
-            InitializeComponent();
-            senderPublicKey = ExecuteClient(publicKey);
-        }
+		public MainWindow()
+		{
+			InitializeComponent();
+			senderPublicKey = ExecuteClient(publicKey);
+		}
 
-        public void CheckFile_Click(object sender, RoutedEventArgs e)
-        {
-            string orignalLines;
-            orignalLines = File.ReadAllText(path.Text);
+		public void CheckFile_Click(object sender, RoutedEventArgs e)
+		{
+			string orignalLines;
+			orignalLines = File.ReadAllText(path.Text);
 
-            string signPath = @"C:\Users\Aymen\Desktop\exx1.txt";
+			string signPath = @"C:\Users\Aymen\Desktop\exx1.txt";
 
-            string signLines;
-            signLines = File.ReadAllText(signPath);
-            Console.WriteLine("data read " + signLines);
+			string signLines;
+			signLines = File.ReadAllText(signPath);
+			Console.WriteLine("data read " + signLines);
 
 
-            var senderRsa = new RSACryptoServiceProvider();
-            senderRsa.FromXmlString(senderPublicKey);
+			var senderRsa = new RSACryptoServiceProvider();
+			senderRsa.FromXmlString(senderPublicKey);
 
-            bool success = VerifyData(orignalLines, signLines, senderRsa.ExportParameters(false));
+			bool success = VerifyData(orignalLines, signLines, senderRsa.ExportParameters(false));
 
-            if (success)
-            {
-                status.Text = "The file is the same has not been tampered with";
-            }
+			if (success)
+			{
+				status.Text = "The file is the same has not been tampered with";
+			}
 
-            else
-            {
-                status.Text = "The file may have been tampered with";
-            }
+			else
+			{
+				status.Text = "The file may have been tampered with";
+			}
 
-        }
+		}
 
-        private void BrowseFile_Click(object sender, RoutedEventArgs e)
-        {
-            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+		private void BrowseFile_Click(object sender, RoutedEventArgs e)
+		{
+			Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
 
-            Nullable<bool> result = dlg.ShowDialog();
+			Nullable<bool> result = dlg.ShowDialog();
 
-            if (result == true)
-            {
-                // Open document 
-                string filename = dlg.FileName;
-                path.Text = filename;
-            }
-        }
+			if (result == true)
+			{
+				// Open document 
+				string filename = dlg.FileName;
+				path.Text = filename;
+			}
+		}
 
-        public static bool VerifyData(string originalMessage, string signedMessage, RSAParameters publicKey)
-        {
-            bool success = false;
-            using (var rsa = new RSACryptoServiceProvider())
-            {
-                var encoder = new UTF8Encoding();
-                byte[] bytesToVerify = encoder.GetBytes(originalMessage);
-                byte[] signedBytes = Convert.FromBase64String(signedMessage);
-                try
-                {
-                    rsa.ImportParameters(publicKey);
+		public static bool VerifyData(string originalMessage, string signedMessage, RSAParameters publicKey)
+		{
+			bool success = false;
+			using (var rsa = new RSACryptoServiceProvider())
+			{
+				var encoder = new UTF8Encoding();
+				byte[] bytesToVerify = encoder.GetBytes(originalMessage);
+				byte[] signedBytes = Convert.FromBase64String(signedMessage);
+				try
+				{
+					rsa.ImportParameters(publicKey);
 
-                    SHA512Managed Hash = new SHA512Managed();
+					SHA512Managed Hash = new SHA512Managed();
 
-                    byte[] hashedData = Hash.ComputeHash(signedBytes);
+					byte[] hashedData = Hash.ComputeHash(signedBytes);
 
-                    success = rsa.VerifyData(bytesToVerify, CryptoConfig.MapNameToOID("SHA512"), signedBytes);
-                }
-                catch (CryptographicException e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-                finally
-                {
-                    rsa.PersistKeyInCsp = false;
-                }
-            }
-            return success;
-        }
+					success = rsa.VerifyData(bytesToVerify, CryptoConfig.MapNameToOID("SHA512"), signedBytes);
+				}
+				catch (CryptographicException e)
+				{
+					Console.WriteLine(e.Message);
+				}
+				finally
+				{
+					rsa.PersistKeyInCsp = false;
+				}
+			}
+			return success;
+		}
 
-        static string ExecuteClient(string text)
-        {
+		static string ExecuteClient(string text)
+		{
 
-            try
-            {
+			try
+			{
 
-                // Establish the remote endpoint  
-                // for the socket. This example  
-                // uses port 11111 on the local  
-                // computer. 
-                IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
-                IPAddress ipAddr = ipHost.AddressList[0];
-                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+				// Establish the remote endpoint  
+				// for the socket. This example  
+				// uses port 11111 on the local  
+				// computer. 
+				IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+				IPAddress ipAddr = ipHost.AddressList[0];
+				IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
 
-                // Creation TCP/IP Socket using  
-                // Socket Class Costructor 
-                Socket sender = new Socket(ipAddr.AddressFamily,
-                           SocketType.Stream, ProtocolType.Tcp);
+				// Creation TCP/IP Socket using  
+				// Socket Class Costructor 
+				Socket sender = new Socket(ipAddr.AddressFamily,
+						   SocketType.Stream, ProtocolType.Tcp);
 
-                try
-                {
+				try
+				{
 
-                    // Connect Socket to the remote  
-                    // endpoint using method Connect() 
-                    sender.Connect(localEndPoint);
+					// Connect Socket to the remote  
+					// endpoint using method Connect() 
+					sender.Connect(localEndPoint);
 
-                    // We print EndPoint information  
-                    // that we are connected 
-                    Console.WriteLine("Socket connected to -> {0} ",
-                                  sender.RemoteEndPoint.ToString());
+					// We print EndPoint information  
+					// that we are connected 
+					Console.WriteLine("Socket connected to -> {0} ",
+								  sender.RemoteEndPoint.ToString());
 
-                    // Creation of messagge that 
-                    // we will send to Server 
-                    byte[] messageSent = Encoding.ASCII.GetBytes(text + "<EOF>");
-                    int byteSent = sender.Send(messageSent);
+					// Creation of messagge that 
+					// we will send to Server 
+					byte[] messageSent = Encoding.ASCII.GetBytes(text + "<EOF>");
+					int byteSent = sender.Send(messageSent);
 
-                    // Data buffer 
-                    byte[] messageReceived = new byte[1024];
+					// Data buffer 
+					byte[] messageReceived = new byte[1024];
 
-                    // We receive the messagge using  
-                    // the method Receive(). This  
-                    // method returns number of bytes 
-                    // received, that we'll use to  
-                    // convert them to string 
-                    int byteRecv = sender.Receive(messageReceived);
-                    string mesRec = Encoding.ASCII.GetString(messageReceived,
-                                                     0, byteRecv);
-                    Console.WriteLine("Message from Server -> {0}", mesRec);
+					// We receive the messagge using  
+					// the method Receive(). This  
+					// method returns number of bytes 
+					// received, that we'll use to  
+					// convert them to string 
+					int byteRecv = sender.Receive(messageReceived);
+					string mesRec = Encoding.ASCII.GetString(messageReceived,
+													 0, byteRecv);
+					Console.WriteLine("Message from Server -> {0}", mesRec);
 
-                    // Close Socket using  
-                    // the method Close() 
-                    sender.Shutdown(SocketShutdown.Both);
-                    sender.Close();
-                    return mesRec;
-                }
+					// Close Socket using  
+					// the method Close() 
+					sender.Shutdown(SocketShutdown.Both);
+					sender.Close();
+					return mesRec;
+				}
 
-                // Manage of Socket's Exceptions 
-                catch (ArgumentNullException ane)
-                {
+				// Manage of Socket's Exceptions 
+				catch (ArgumentNullException ane)
+				{
 
-                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
-                    return null;
-                }
+					Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+					return null;
+				}
 
-                catch (SocketException se)
-                {
+				catch (SocketException se)
+				{
 
-                    Console.WriteLine("SocketException : {0}", se.ToString());
-                    return null;
-                }
+					Console.WriteLine("SocketException : {0}", se.ToString());
+					return null;
+				}
 
-                catch (Exception e)
-                {
-                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
-                    return null;
-                }
-            }
+				catch (Exception e)
+				{
+					Console.WriteLine("Unexpected exception : {0}", e.ToString());
+					return null;
+				}
+			}
 
-            catch (Exception e)
-            {
+			catch (Exception e)
+			{
 
-                Console.WriteLine(e.ToString());
-                return null;
-            }
-        }
-    }
+				Console.WriteLine(e.ToString());
+				return null;
+			}
+		}
 
-    
+		private void ImagePanel_Drop(object sender, DragEventArgs e) {
+			Console.WriteLine("In here");
+			//MessageBox.Show(e.Data.GetData(DataFormats.FileDrop, false)[0])
+			string[] fileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+			path.Text = fileList[0];
+		}
+	}
+
+	
 }
